0919ff18cb90de816a235cab46c60077c91d166a
yyang2 <yyang2+git@gmail.com>
2011-06-13 16:43:39 -0700

    added header

---
+++ 3DROIManager.xcodeproj/SwizzleOrthogonalMPRPETCTViewer.h
@@ -0,0 +1,16 @@
+//
+//  SwizzleOrthoMPRPETCTController.h
+//  3DROIManager
+//
+//  Created by Yang Yang on 6/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <OsirixAPI/OrthogonalMPRPETCTViewer.h>
+
+@interface SwizzleOrthogonalMPRPETCTViewer : OrthogonalMPRPETCTViewer {
+}
+- (void) setMovieIndex: (short) i;
+
+@end
